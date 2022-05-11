bb1e6c31aedf94b03386368c1ebf5de0904e0c9a
thermidor <lanya26@naver.com>
2022-05-11 16:32:50 +0900

    모델 생성 마이그레이션 마이그레이트

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'firstapp',
-
+    'secondapp',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- secondapp/views.py
+++ secondapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Course
+
 def main(request):
     return HttpResponse(
         '<h1><u>Main</u></h1>'
