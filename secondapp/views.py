fda124a9ff24877af30b20d8e564991aa4521e69
thermidor <lanya26@naver.com>
2022-05-11 18:20:45 +0900

    Template 활용

--- secondapp/views.py
+++ secondapp/views.py
@@ -5,10 +5,11 @@ from .models import Course
 
 def show(request):
     course = Course.objects.all()
-    result = ''
-    for c in course:
-        result += c.name + '<br>'
-    return HttpResponse(result)
+    # # result = ''
+    # # for c in course:
+    # #     result += c.name + '<br>'
+    # return HttpResponse(result)
+    return render(request, 'show.html', { 'data': course })
 
 def insert(request):
     Course(name = '데이터 분석', cnt = 30).save()
